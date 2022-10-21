6c6a797acc8bb241cd009d5dfa716213a1864ac7
Ben (X13/Arch) <ben@wolkenwelten.net>
2022-10-21 11:47:49 +0200

    Improved startup/binary size

--- README.md
+++ README.md
@@ -40,8 +40,8 @@ down a bit I'll be looking into integrating v8, by either using deno_core or hoo
 - [X] Proper chunk draw ordering, front to back due to fade-in (port from WW)
 - [X] Chunk fade-in after generation
 - [ ] Sky sphere (port from WW)
-- [ ] Frame-rate independent physics/gameplay (maybe try out rapier3d?)
 - [ ] Greedy meshing (port from WW)
 - [ ] Lighting (port from WW, without ASM/SIMD (for now))
+- [ ] Frame-rate independent physics/gameplay (maybe try out rapier3d?)
 - [ ] Block manipulation (simple removal/placement as well as block selection)
 - [ ] Block highlight (port from WW)
--- client/src/texture.rs
+++ client/src/texture.rs
@@ -79,20 +79,10 @@ impl Texture {
 
 impl TextureArray {
     pub fn from_bytes(label: &str, bytes: &'static [u8]) -> Result<Self, image::ImageError> {
-        let mut img = image::load_from_memory(bytes)?;
-        let tile_size: u32 = img.width() / 16;
+        let img = image::load_from_memory(bytes)?;
+        let tile_size: u32 = img.width();
+        let tile_count = img.height() / tile_size;
 
-        let mut atlas = image::RgbaImage::new(tile_size, tile_size * 256);
-        for y in 0..16 {
-            for x in 0..16 {
-                let i = x + (y * 16);
-                let tile = img.crop(x * tile_size, y * tile_size, tile_size, tile_size);
-                let tile = tile.flipv();
-                image::imageops::replace(&mut atlas, &tile, 0, (i * tile_size).into());
-            }
-        }
-
-        let img = image::DynamicImage::from(atlas);
         let img = match img {
             image::DynamicImage::ImageRgba8(img) => img,
             x => x.to_rgba8(),
@@ -122,7 +112,7 @@ impl TextureArray {
                 gl::RGBA.try_into().unwrap(),
                 tile_size.try_into().unwrap(),
                 tile_size.try_into().unwrap(),
-                256,
+                tile_count.try_into().unwrap(),
                 0,
                 gl::RGBA,
                 gl::UNSIGNED_BYTE,
